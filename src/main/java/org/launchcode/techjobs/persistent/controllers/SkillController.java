db03f48b34313e96d64ae4ae8973fc65b4f4a08d
kalenduhs <kalenduhs@gmail.com>
2024-01-25 18:25:55 -0600

    finishing task two

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -0,0 +1,11 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("skills")
+public class SkillController {
+
+    //finish Skill Controller
+}
